e1a7f4511157e8bce36b6b297a28c79780f92305
Kamil <kamil@czanik.xyz>
2019-05-02 14:16:52 +0200

    OnRecyclerItemClick impl

--- app/src/main/java/xyz/czanik/indoorway_countries/countries/CountriesPresenter.kt
+++ app/src/main/java/xyz/czanik/indoorway_countries/countries/CountriesPresenter.kt
@@ -12,8 +12,7 @@ class CountriesPresenter @Inject constructor(
 
     val recyclerItemClickListener = object : OnItemClickListener {
         override fun onItemClick(itemPos: Int) {
-            //view.startSingleCountryActivityFor(model.countries[itemPos].name)
-            view.showMessage("Hello $itemPos")
+            view.startSingleCountryActivityFor(view.recyclerAdapter.countries[itemPos].name)
         }
     }
 
