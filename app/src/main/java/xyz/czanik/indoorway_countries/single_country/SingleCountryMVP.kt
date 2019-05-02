2729a24e5039623a9aca7180028eb8d04d6d7aea
Kamil <kamil@czanik.xyz>
2019-05-02 14:56:21 +0200

    SingleCountryMVP contract

---
+++ app/src/main/java/xyz/czanik/indoorway_countries/single_country/SingleCountryMVP.kt
@@ -0,0 +1,23 @@
+package xyz.czanik.indoorway_countries.single_country
+
+import xyz.czanik.indoorway_countries.OnLoadingCompleteListener
+
+interface SingleCountryMVP {
+
+    interface View {
+        val countryName: String
+
+        fun showMessage(message: String)
+    }
+
+    interface Presenter {
+        val view: View
+        val model: Model
+
+        fun prepareView()
+    }
+
+    interface Model {
+        fun loadCountryData(countryName: String,loadingListener: OnLoadingCompleteListener)
+    }
+}
