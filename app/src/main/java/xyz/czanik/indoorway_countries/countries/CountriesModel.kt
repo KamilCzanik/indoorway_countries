5b7dc05c0a6f81f6a418f5a02a2ad38c520975c1
Kamil <kamil@czanik.xyz>
2019-05-02 14:16:25 +0200

    refactoring

--- app/src/main/java/xyz/czanik/indoorway_countries/countries/CountriesModel.kt
+++ app/src/main/java/xyz/czanik/indoorway_countries/countries/CountriesModel.kt
@@ -1,8 +1,8 @@
 package xyz.czanik.indoorway_countries.countries
 
 import android.content.Context
-import android.util.Log
 import com.android.volley.Request
+import com.android.volley.Request.Method.GET
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
@@ -12,21 +12,28 @@ import javax.inject.Inject
 
 class CountriesModel @Inject constructor(private val context: Context) : CountriesMVP.Model {
     override val countries = ArrayList<CountryItem>()
-    private val countriesUrl = "https://restcountries.eu/rest/v2/all?fields=name;flag"
+
+    private val NAME = "name"
+    private val FLAG = "flag"
+    private val countriesUrl = "https://restcountries.eu/rest/v2/all?fields=$NAME;$FLAG"
 
     override fun loadCountries(loadingListener: CountriesMVP.Model.OnLoadingCompleteListener) {
-        val request = JsonArrayRequest(Request.Method.GET,countriesUrl,null,
+        val request = JsonArrayRequest(
+            GET,
+            countriesUrl,
+            null,
             Response.Listener<JSONArray> { jsonArray ->
                 try {
                     for(i in 0 until jsonArray.length()) {
                         val country = jsonArray.getJSONObject(i)
-                        val name = country.getString("name")
-                        val flag = country.getString("flag")
+                        val name = country.getString(NAME)
+                        val flag = country.getString(FLAG)
                         countries += CountryItem(name,flag)
                     }
                     loadingListener.onComplete()
                 } catch (e: JSONException) { loadingListener.onFailure(e.message!!) }
-            },Response.ErrorListener { loadingListener.onFailure(it.message!!) })
+            },
+            Response.ErrorListener { loadingListener.onFailure(it.message!!) })
 
         Volley.newRequestQueue(context).add(request)
     }
