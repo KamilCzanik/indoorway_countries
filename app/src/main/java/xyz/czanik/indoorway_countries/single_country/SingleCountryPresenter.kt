148ca528ac3317003d3cc7c607cab1ebbe6b43ea
Kamil <kamil@czanik.xyz>
2019-05-03 13:33:30 +0200

    Code formatting

--- app/src/main/java/xyz/czanik/indoorway_countries/single_country/SingleCountryPresenter.kt
+++ app/src/main/java/xyz/czanik/indoorway_countries/single_country/SingleCountryPresenter.kt
@@ -8,16 +8,11 @@ class SingleCountryPresenter @Inject constructor(
     override val model: SingleCountryMVP.Model) : SingleCountryMVP.Presenter {
 
     val loadingListener = object : OnLoadingCompleteListener {
-        override fun onComplete() {
-            view.country = model.country
-        }
 
-        override fun onFailure(message: String) {
-            view.showMessage(message)
-        }
+        override fun onComplete() { view.country = model.country }
+
+        override fun onFailure(message: String) { view.showMessage(message) }
     }
 
-    override fun prepareView() {
-        model.loadCountryData(view.countryCode,loadingListener)
-    }
+    override fun prepareView() { model.loadCountryData(view.countryCode,loadingListener) }
 }
