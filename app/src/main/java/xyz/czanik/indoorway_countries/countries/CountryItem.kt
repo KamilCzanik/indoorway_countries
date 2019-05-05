22eee968fdf8195eca48c8c92f9e9228b00ca84c
Kamil <kamil@czanik.xyz>
2019-05-05 12:40:33 +0200

    Default values and prepare for json parsing

--- app/src/main/java/xyz/czanik/indoorway_countries/countries/CountryItem.kt
+++ app/src/main/java/xyz/czanik/indoorway_countries/countries/CountryItem.kt
@@ -1,4 +1,9 @@
 package xyz.czanik.indoorway_countries.countries
 
+import com.beust.klaxon.Json
+
 /** Klasa przechowująca podstawowe informacje potrzebne do wyświetlenie przedmiotu w recyclerView*/
-data class CountryItem(val name: String, val flagUri: String, val code: String)
+data class CountryItem(
+    val name: String = "",
+    val flag: String = "",
+    @Json(name = "alpha3Code") val code: String = "")
--- app/src/main/java/xyz/czanik/indoorway_countries/single_country/ComplexCountry.kt
+++ app/src/main/java/xyz/czanik/indoorway_countries/single_country/ComplexCountry.kt
@@ -1,20 +1,23 @@
 package xyz.czanik.indoorway_countries.single_country
 
+import com.beust.klaxon.Json
 import com.google.android.gms.maps.model.LatLng
 
 /** Klasa przechowująca podstawowe informacje potrzebne do wyświetlenie informacji kraju w aktywności*/
 data class ComplexCountry(
     val name: String = "",
-    val flagUri: String = "",
-    val code: String = "",
-    val capitalCity: String = "",
+    val flag: String = "",
+    @Json(name = "alpha3Code") val code: String = "",
+    val capital: String = "",
     val region: String = "",
     val population: String = "",
-    val latLng: LatLng = LatLng(0.0,0.0)) {
+    val latlng: DoubleArray = doubleArrayOf(0.0,0.0)){
+
+    fun getLatLng() = LatLng(latlng[0],latlng[1])
 
     val details =
                 " Code : $code \n" +
-                " Capital city : $capitalCity \n" +
+                " Capital city : $capital \n" +
                 " Region : $region \n" +
                 " Population : $population"
 }
