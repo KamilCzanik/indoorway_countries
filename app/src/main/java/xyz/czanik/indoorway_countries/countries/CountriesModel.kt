3e1f4121601db523772fe1d432d0e3d813763081
Kamil <kamil@czanik.xyz>
2019-05-05 12:41:18 +0200

    Json parsing with Klaxon

--- app/src/main/java/xyz/czanik/indoorway_countries/countries/CountriesModel.kt
+++ app/src/main/java/xyz/czanik/indoorway_countries/countries/CountriesModel.kt
@@ -5,21 +5,18 @@ import com.android.volley.Request.Method.GET
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
+import com.beust.klaxon.Klaxon
 import org.json.JSONArray
 import org.json.JSONException
-import org.json.JSONObject
 import xyz.czanik.indoorway_countries.OnLoadingCompleteListener
+import xyz.czanik.indoorway_countries.toJsonString
 import javax.inject.Inject
 
 /** Klasa odpowiedzialna za dostarczenie danych pobranych z REST API*/
 class CountriesModel @Inject constructor(private val context: Context) : CountriesMVP.Model {
 
     override val countries = ArrayList<CountryItem>()
-
-    private val NAME = "name"
-    private val FLAG = "flag"
-    private val CODE = "alpha3Code"
-    private val countriesUrl = "https://restcountries.eu/rest/v2/all?fields=$NAME;$FLAG;$CODE"
+    private val countriesUrl = "https://restcountries.eu/rest/v2/all?fields=name;flag;alpha3Code"
 
     override fun loadCountries(loadingListener: OnLoadingCompleteListener) {
         val request = JsonArrayRequest(
@@ -28,20 +25,16 @@ class CountriesModel @Inject constructor(private val context: Context) : Countri
             null,
             Response.Listener<JSONArray> { jsonArray ->
                 try {
-                    for(i in 0 until jsonArray.length())
-                        countries += parseJsonToCountryItem(jsonArray.getJSONObject(i))
+                    for (i in 0 until jsonArray.length())
+                        countries += Klaxon().parse<CountryItem>(jsonArray.getJSONObject(i).toJsonString()) ?: CountryItem()
 
                     loadingListener.onComplete()
-                } catch (e: JSONException) { loadingListener.onFailure(e.message!!) }
+                } catch (e: JSONException) {
+                    loadingListener.onFailure(e.message!!)
+                }
             },
             Response.ErrorListener { loadingListener.onFailure(it.message!!) })
 
         Volley.newRequestQueue(context).add(request)
     }
-
-    private fun parseJsonToCountryItem(json: JSONObject) =
-        CountryItem(
-        json.getString(NAME),
-        json.getString(FLAG),
-        json.getString(CODE))
 }
--- app/src/main/java/xyz/czanik/indoorway_countries/single_country/SingleCountryModel.kt
+++ app/src/main/java/xyz/czanik/indoorway_countries/single_country/SingleCountryModel.kt
@@ -5,12 +5,11 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import com.google.android.gms.maps.model.LatLng
-import org.json.JSONArray
+import com.beust.klaxon.Klaxon
 import org.json.JSONException
 import org.json.JSONObject
 import xyz.czanik.indoorway_countries.OnLoadingCompleteListener
-import xyz.czanik.indoorway_countries.R
+import xyz.czanik.indoorway_countries.toJsonString
 import javax.inject.Inject
 
 /** Klasa odpowiedzialna za dostarczenie danych pobranych z REST API*/
@@ -18,15 +17,8 @@ class SingleCountryModel @Inject constructor(private val context: Context) : Sin
 
     override lateinit var country: ComplexCountry
 
-    private val NAME = "name"
-    private val FLAG = "flag"
-    private val REGION = "region"
-    private val LATLNG = "latlng"
-    private val CAPITAL = "capital"
-    private val POPULATION = "population"
-
     override fun loadCountryData(countryCode: String, loadingListener: OnLoadingCompleteListener) {
-        val countryUri = "https://restcountries.eu/rest/v2/alpha/$countryCode?fields=$NAME;$FLAG;$REGION;$CAPITAL;$LATLNG;$POPULATION"
+        val countryUri = "https://restcountries.eu/rest/v2/alpha/$countryCode?fields=name;flag;alpha3Code;capital;region;population;latlng"
 
         val request = JsonObjectRequest(
             Request.Method.GET,
@@ -34,7 +26,7 @@ class SingleCountryModel @Inject constructor(private val context: Context) : Sin
             null,
             Response.Listener<JSONObject> { json ->
                 try {
-                    country = parseJsonToComplexCountry(json,countryCode)
+                    country = Klaxon().parse<ComplexCountry>(json.toJsonString()) ?: ComplexCountry()
                     loadingListener.onComplete()
                 } catch (e: JSONException) { loadingListener.onFailure(e.message!!) }
             },
@@ -42,24 +34,4 @@ class SingleCountryModel @Inject constructor(private val context: Context) : Sin
 
         Volley.newRequestQueue(context).add(request)
     }
-
-    private fun parseJsonToComplexCountry(jsonObject: JSONObject, countryCode: String) : ComplexCountry {
-        val default = context.resources.getString(R.string.unknown)
-        return ComplexCountry(
-            jsonObject.getStringOrDefault(NAME,default),
-            jsonObject.getStringOrDefault(FLAG,default),
-            countryCode,
-            jsonObject.getStringOrDefault(CAPITAL,default),
-            jsonObject.getStringOrDefault(REGION,default),
-            jsonObject.getStringOrDefault(POPULATION,default),
-            jsonObject.getJSONArray(LATLNG).toLatLng())
-    }
-
-
 }
-
-/** Opakowanie kodu służącego do wydobycia danych z JSON w celu zwiększenia czytelności*/
-
-fun JSONArray.toLatLng() = if(length() != 0) LatLng(getDouble(0),getDouble(1)) else LatLng(0.0,0.0)
-
-fun JSONObject.getStringOrDefault(key: String,default: String): String = if(getString(key).isNotEmpty()) getString(key) else default
